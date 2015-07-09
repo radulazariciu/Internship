18213d1ff67cc0435b634b7b54fc034dba0d0241
occultscripto <ionut.indre@yahoo.com>
2015-07-09 17:54:26 +0300

    Solved Flicker. Not the best approach.

---
+++ Nike-SolvedWhiteFlicker/js/index.js
@@ -0,0 +1,41 @@
+$(document).ready(function() { 
+	var helpButton = document.getElementById("topNavbarButtons8");
+	var topDDL = document.getElementById("topDropDown");
+	
+	helpButton.addEventListener("mouseover", helpHover);
+	helpButton.addEventListener("click", helpClick);
+	helpButton.addEventListener("mouseout", helpMouseOut);
+	topDDL.addEventListener("mouseover", topDDLover);
+	topDDL.addEventListener("mouseout", topDDLoverOut);
+
+	function helpHover() {	
+		$("#topNavbar").find('.dropdown-menu').stop(false, false).delay(0).fadeIn(0);
+	}
+
+	function helpClick() {
+		location.href = "help.html";		
+	}
+
+	setTimeout(helpMouseOut, 4000);
+
+	function helpMouseOut() {			
+		$("#topNavbar").find('.dropdown-menu').stop(false, false).delay(2000).fadeOut(0);		
+	}
+
+	function topDDLover() {	
+		$("#topNavbar").find('.dropdown-menu').stop(false, false).delay(0).fadeIn(0);	
+		$("#topNavbar").find('.dropdown-menu').stop(false, false).delay(0).fadeIn(0);
+		$("#topNavbar").find('.dropdown-menu').stop(false, false).delay(0).fadeIn(0);
+		$("#topNavbar").find('.dropdown-menu').stop(false, false).delay(0).fadeIn(0);
+		$("#topNavbar").find('.dropdown-menu').stop(false, false).delay(0).fadeIn(0);
+		$("#topNavbar").find('.dropdown-menu').stop(false, false).delay(0).fadeIn(0);
+		$("#topNavbar").find('.dropdown-menu').stop(false, false).delay(0).fadeIn(0);
+		$("#topNavbar").find('.dropdown-menu').stop(false, false).delay(0).fadeIn(0);
+		$("#topNavbar").find('.dropdown-menu').stop(false, false).delay(0).fadeIn(0);
+		$("#topNavbar").find('.dropdown-menu').stop(false, false).delay(0).fadeIn(0);			
+	}
+
+	function topDDLoverOut() {
+		$("#topNavbar").find('.dropdown-menu').stop(false, false).delay(2000).fadeOut(0);
+	}
+});
