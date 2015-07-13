3c7f801430bb71c9a5939ee8ca300d586eb5a182
occultscripto <ionut.indre@yahoo.com>
2015-07-13 18:04:07 +0300

    Input Forms MVC

---
+++ TestControllersAndViews/TestControllersAndViews/Controllers/inController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestControllersAndViews.Controllers
+{
+    public class inController : Controller
+    {
+        //
+        // GET: /in/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
