5128e4cd284b9902d8f646bab3c3fd97ddda7cf1
bogdanmates <matesbogdan92@gmail.com>
2015-07-13 11:06:31 +0300

    mvc example

---
+++ MVC/someExample/WebApplication2/WebApplication2/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+
+        public string Address { get; set; }
+
+        public int Age { get; set; }
+
+    }
+}
---
+++ MVC/someExample/WebApplication2/WebApplication2/Models/CustomerBAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class CustomerBAL
+    {
+        public Customer GetCustomer()
+        {
+            Customer c = new Customer();
+            c.CustomerName = "James";
+            c.Address = "UK";
+            c.Age = 25;
+            return c;
+        }
+    }
+}
