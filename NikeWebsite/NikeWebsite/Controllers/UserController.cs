6ffa678e66229d16a39de6dd8d0a6cd96704c846
bogdanmates <matesbogdan92@gmail.com>
2015-07-14 13:32:18 +0300

    login under construction

--- NikeWebsite/NikeWebsite/Controllers/UserController.cs
+++ NikeWebsite/NikeWebsite/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using NikeWebsite.Models;
+using WebMatrix.WebData;
 
 namespace NikeWebsite.Controllers
 {
@@ -52,6 +53,44 @@ namespace NikeWebsite.Controllers
                 return View();
             }
         }
+
+
+
+
+        //
+        // GET: /Account/Login
+
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        //
+        // POST: /Account/Login
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid && WebSecurity.Login(model.UserName, model.Password, persistCookie: model.RememberMe))
+            {
+                return RedirectToLocal(returnUrl);
+            }
+
+            // If we got this far, something failed, redisplay form
+            ModelState.AddModelError("", "The user name or password provided is incorrect.");
+            return View(model);
+        }
+
+
+
+
+
+
+
 
         //
         // GET: /User/Edit/5
