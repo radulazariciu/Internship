97db3e634aac892dfe8073be9da2801980987bea
bogdanmates <matesbogdan92@gmail.com>
2015-07-14 12:13:27 +0300

    user and user mapper done

--- NikeWebsite/NikeWebsite/DataMappers/UserMapper.cs
+++ NikeWebsite/NikeWebsite/DataMappers/UserMapper.cs
@@ -8,10 +8,44 @@ namespace NikeWebsite.Models.DataMappers
     public class UserMapper
     {
         //lista
+        public static List<User> theUsers = new List<User>
+        {
+            new User
+            {
+                Id = 0,
+                Name = "James",
+                Password = "James"
+            },
+
+            new User
+            {
+                Id = 1,
+                Name = "John",
+                Password = "John"
+            },
+
+            new User
+            {
+                Id = 2,
+                Name = "Jim",
+                Password = "Jim"
+            }
+        };
 
         //add user
+        public void addUser(User u)
+        {
+            theUsers.Add(u);
+        }
 
         //check if in list
-
+        public bool checkUserLogin(User u)
+        {
+            if (theUsers.Contains(u))
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
--- NikeWebsite/NikeWebsite/Models/User.cs
+++ NikeWebsite/NikeWebsite/Models/User.cs
@@ -7,5 +7,8 @@ namespace NikeWebsite.Models
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
     }
 }
