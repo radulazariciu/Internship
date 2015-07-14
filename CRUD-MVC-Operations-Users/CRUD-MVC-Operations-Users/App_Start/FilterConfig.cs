88e733912c7fd449e56c94f6fe867a5f49ca79fa
occultscripto <ionut.indre@yahoo.com>
2015-07-14 18:27:16 +0300

    Alternative user DAO

---
+++ CRUD-MVC-Operations-Users/CRUD-MVC-Operations-Users/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_MVC_Operations_Users
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
