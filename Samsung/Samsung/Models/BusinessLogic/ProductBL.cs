61ed5c256522939faab8e5c2898cd95c81fb89f7
occultscripto <ionut.indre@yahoo.com>
2015-07-14 12:03:55 +0300

    Added achitectural design for Samsung.

---
+++ Samsung/Samsung/Models/BusinessLogic/ProductBL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Samsung.Controllers.BusinessLogic
+{
+    public class ProductBL
+    {
+    }
+}
