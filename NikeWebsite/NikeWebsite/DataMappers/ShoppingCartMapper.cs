e48eb95058a1b0eef8624c5e76be9f8d8184b71d
Cristi Cioban <ciobancristi@gmail.com>
2015-07-14 14:37:02 +0300

    MVC product ops

--- NikeWebsite/NikeWebsite/DataMappers/ShoppingCartMapper.cs
+++ NikeWebsite/NikeWebsite/DataMappers/ShoppingCartMapper.cs
@@ -7,5 +7,23 @@ namespace NikeWebsite.Models.DataMappers
 {
     public class ShoppingCartMapper
     {
+        private ShoppingCart _shoppingCart = new ShoppingCart();
+
+        public List<Product> GetProducts()
+        {
+            return _shoppingCart.Products;
+        }
+        public void AddProduct(Product p)
+        {
+            _shoppingCart.Products.Add(p);
+        }
+        public void RemoveProduct(int id)
+        {
+            var product = _shoppingCart.Products.FirstOrDefault(prod => prod.Id == id);
+            if (product != null)
+            {
+                _shoppingCart.Products.Remove(product);
+            }
+        }
     }
 }
