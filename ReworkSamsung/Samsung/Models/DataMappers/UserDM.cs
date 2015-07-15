5b112892f41888b0a2217a4262972487b0e1e570
occultscripto <ionut.indre@yahoo.com>
2015-07-15 13:46:13 +0300

    Added log fields

---
+++ ReworkSamsung/Samsung/Models/DataMappers/UserDM.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Samsung.Models;
+using Samsung.Models.Validators;
+
+namespace Samsung.Models.DataMappers
+{
+    public class UserDM
+    {
+        public static List<User> usersList;
+
+        public UserDM()
+        {
+            usersList = new List<User>();
+            usersList.Add(new User("Andrei","Toader",new UserDateOfBirth("January","1994","18"),"tdandrey94@yahoo.com",new UserCellPhone("074","261","3662"),"242","password"));
+            usersList.Add(new User("Andrei","Toader",new UserDateOfBirth("January","1994","18"),"tdandrey94@gmail.com",new UserCellPhone("074","261","3662"),"242","password"));
+        }
+
+        public void Add(User User)
+        {
+            try
+            {
+                UserValidator userValidator = new UserValidator();
+                userValidator.Validate(User);
+                usersList.Add(User);
+            }
+            catch (InvalidUserException invalidUserException)
+            {
+                throw invalidUserException;
+            }
+        }
+
+        public List<User> GetAllUsers()
+        {
+            List<User> usersListToBeReturned = new List<User>();
+            for (int index = 0; index < usersList.Count; index++)
+            {
+                usersListToBeReturned.Add(usersList[index]);
+            }
+            return usersListToBeReturned;
+        }
+    }
+}
---
+++ Samsung - REWORK/Samsung/Models/Validators/InvalidUserException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Samsung.Models.Validators
+{
+    public class InvalidUserException :Exception
+    {
+        private string exceptionMessage;
+        public InvalidUserException(string Message)
+            :base(Message)
+        {
+            this.exceptionMessage = Message;
+        }
+
+        public string Message 
+        { 
+            get
+            {
+                return this.exceptionMessage;
+            }  
+        }
+    }
+}
