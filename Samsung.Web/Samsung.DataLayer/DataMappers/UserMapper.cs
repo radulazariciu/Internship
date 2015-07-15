49f11b7b7b8bd377eba15de7c26ae8ac60451bd8
occultscripto <ionut.indre@yahoo.com>
2015-07-16 00:33:27 +0300

    Added MVC Arhchitecture. Solved register and login. Make required fields.

---
+++ Samsung.Web/Samsung.DataLayer/DataMappers/UserMapper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Samsung.DataLayer.Models;
+
+namespace Samsung.DataLayer.DataMappers
+{
+    public class UserMapper
+    {
+   
+        public List<User> usersList = new List<User>
+        {
+            new User
+            {
+               Id = 0,
+               FirstName = "TestFM",
+               LastName = "TestSN",
+               Password = "test",
+               EmailAddress = "test@test.com",
+               Zipcode = 12345,
+               DateOfBirth = DateTime.Today
+            }
+        };
+
+        public void AddUser(User user)
+        {
+            usersList.Add(user);
+        }
+
+        public Boolean CheckUserExistence(User user)
+        {
+            foreach (var item in usersList)
+            {
+                if (item.EmailAddress.Equals(user.EmailAddress))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return usersList;
+        }
+    }
+}
