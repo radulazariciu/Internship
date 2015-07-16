00f605ed191198e564fcceeb04bde394e5da171a
occultscripto <ionut.indre@yahoo.com>
2015-07-16 15:51:56 +0300

    Added new methods in BL for shoppingcart

--- Samsung.Web/Samsung.DataLayer/DataMappers/ShoppingCartMapper.cs
+++ Samsung.Web/Samsung.DataLayer/DataMappers/ShoppingCartMapper.cs
@@ -8,92 +8,41 @@ namespace Samsung.DataLayer.DataMappers
 {
     public class ShoppingCartMapper
     {
-        private List<ShoppingCartItem> shoppingCartList;
+        private ShoppingCart shoppingCart;
+
+        public void SetUserToShoppingCart(User user)
+        {
+            shoppingCart.User = user;
+        }
 
         public ShoppingCartMapper()
         {
-            if (shoppingCartList == null)
+            if (shoppingCart == null)
             {
-                shoppingCartList = new List<ShoppingCart>();
-                Init();
+                shoppingCart = new ShoppingCart();
             }
         }
 
-        private void Init()
+        public ShoppingCart GetShoppingCart()
         {
-            var p = new Product();
-            p.Id = 2;
-            p.Name = "Samsung Galaxy Note Edge";
-            p.Category = "T-Mobile";
-            p.Color = "black";
-            p.Price = 550;
-            p.Stock = 10;
-
-            var p1 = new Product();
-            p1.Id = 3;
-            p1.Name = "Samsung Galaxy Note II ";
-            p1.Category = "Sprint";
-            p1.Color = "gray";
-            p.Price = 850;
-            p.Stock = 7;
-
-            var sc = new ShoppingCart();
-            sc.Id = 1;
-            sc.ShoppingCartItemsList = new List<ShoppingCartItem>();
-            var it = new ShoppingCartItem();
-            it.IdProduct = 1;
-            it.Quantity = 1;
-            sc.ShoppingCartItemsList.Add(it);
-            shoppingCartList.Add(sc);
-
-            sc = new ShoppingCart();
-            sc.Id = 2;
-            sc.ShoppingCartItemsList = new List<ShoppingCartItem>();
-
-            it = new ShoppingCartItem();
-            it.IdProduct = 1;
-            it.Quantity = 2;
-            sc.ShoppingCartItemsList.Add(it);
-
-            it = new ShoppingCartItem();
-            it.IdProduct = 2;
-            it.Quantity = 1;
-            sc.ShoppingCartItemsList.Add(it);
-            shoppingCartList.Add(sc);
+            return shoppingCart;
         }
 
-        public List<ShoppingCart> GetAllShoppingCart()
+        public ShoppingCartItem GetShoppingCartItem(int idProduct)
         {
-            return shoppingCartList;
+            var item = shoppingCart.ShoppingCartItemsList.Find(i => i.IdProduct == idProduct);
+            return item;
         }
 
-        public ShoppingCart GetShoppingCart(int id)
+        public void RemoveShoppingCartItem(int idProduct)
         {
-            var sh = shoppingCartList.Find(i => i.Id == id);
-            return sh;
+            var item = shoppingCart.ShoppingCartItemsList.Find(i => i.IdProduct == idProduct);
+            shoppingCart.ShoppingCartItemsList.Remove(item);
         }
 
-        public void RemoveShoppingCart(int id)
+        public void AddShoppingCartItem(ShoppingCartItem sc)
         {
-            var cart = shoppingCartList.Find(c => c.Id == id);
-            shoppingCartList.Remove(cart);
-        }
-
-        public void AddShoppingCart(ShoppingCart sc)
-        {
-            shoppingCartList.Add(sc);
-        }
-
-        public void AddItemToUserCart(ShoppingCartItem i, User u)
-        {
-
-            var cart = shoppingCartList.Find(x => x.User.EmailAddress == u.EmailAddress);
-            if (cart == null)
-            {
-                cart = new ShoppingCart();
-                cart.User = u;
-                cart.ShoppingCartItemsList.Add(i);
-            }
+            shoppingCart.ShoppingCartItemsList.Add(sc);
         }
     }
 }
--- Samsung.Web/Samsung.Web/Controllers/ShoppingCartController.cs
+++ Samsung.Web/Samsung.Web/Controllers/ShoppingCartController.cs
@@ -39,4 +39,5 @@ namespace Samsung.Web.Controllers
     }
 
     }
+
 }
