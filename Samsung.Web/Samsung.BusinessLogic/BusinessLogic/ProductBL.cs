35f1b83ec4485c8fbd58a3f4436d925bbcceccb5
occultscripto <ionut.indre@yahoo.com>
2015-07-16 09:45:57 +0300

    Added product model and mapper

--- Samsung.Web/Samsung.BusinessLogic/BusinessLogic/ProductBL.cs
+++ Samsung.Web/Samsung.BusinessLogic/BusinessLogic/ProductBL.cs
@@ -2,10 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Samsung.BusinessLogic.ViewModels;
+using Samsung.DataLayer.DataMappers;
+using Samsung.DataLayer.Models;
+using Samsung.BusinessLogic.ViewModels;
 
 namespace Samsung.BusinessLogic.BusinessLogic
 {
-    public class ProductBL
+    public static class ProductBL
     {
+        public static List<ProductVM> GetAllProducts()
+        {
+        }
+
+        public static GetProduct(int id)
+        {
+             ProductMapper userMapper = new UserMapper();
+            return _productDataMapper.GetProduct(id);
+        }
+
+        publicstatic List<Product> CategoryProducts()
+        {
+            return _productDataMapper.GetAllProducts();
+        }
+
+        public void Edit(ProductVM productVM)
+        {
+            _productDataMapper.Edit(p);
+        }
     }
 }
--- Samsung.Web/Samsung.DataLayer/Models/Product.cs
+++ Samsung.Web/Samsung.DataLayer/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,13 @@ namespace Samsung.DataLayer.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Color { get; set; }
+        public Image Image { get; set; }
+        public List<string> Details { get; set; }
+        public float Price { get; set; }
+        public int Stock { get; set; }
     }
 }
