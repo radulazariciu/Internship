530432c251683698478e9067d54c8449d56b2820
andrei.toader <andrei.toader@techromix.com>
2015-07-17 18:03:57 +0300

    Manage the list of products (add/update/delete)-not integrated yet in final project

---
+++ Samsung.Web-ManageProductsNotIntegrated/Samsung.BusinessLogic/ViewModels/UserVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Samsung.BusinessLogic.ViewModels
+{
+    public class UserVM
+    {
+        public UserVM(string emailAddress, string password)
+        {
+            EmailAddress = emailAddress;
+            Password = password;
+        }
+
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Samsung.Web-ManageProductsNotIntegrated/Samsung.DataLayer/Models/Product.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace Samsung.DataLayer.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Color { get; set; }
+        public string ImagePath { get; set; }
+        public string Details { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; }
+
+        public Product(int id, string name, string category, string color, string image, string details, int price, int stock)
+        {
+            Id = id;
+            Name = name;
+            Category = category;
+            Color = color;
+            ImagePath = image;
+            Details = details;
+            Price = price;
+            Stock = stock;
+        }
+
+        public Product(int id, string name, string category, string color, string details, int price, int stock)
+        {
+            Id = id;
+            Name = name;
+            Category = category;
+            Color = color;
+            Details = details;
+            Price = price;
+            Stock = stock;
+        }
+
+        public Product()
+        {
+            
+        }
+
+    }
+ 
+}
