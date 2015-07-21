b515a6d311c65e764a98bc0e7a48289f75d5a662
occultscripto <ionut.indre@yahoo.com>
2015-07-21 12:58:02 +0300

    Added MVC with EPISERVER Functionality.

---
+++ EPiServerSiteSamsungWork/EPiServerSiteSamsungWork/Models/Pages/StartPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace EPiServerSiteSamsungWork.Models.Pages
+{
+    [ContentType(DisplayName = "StartPage", GUID = "8f4259f8-9218-4a72-a6f1-e27092caa30a", Description = "")]
+    public class StartPage : PageData
+    {
+        
+                [CultureSpecific]
+                [Display(
+                    Name = "Main body",
+                    Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual XhtmlString MainBody { get; set; }
+         
+    }
+}
